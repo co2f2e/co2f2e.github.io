4b02d99b7dd5b534b2555051ab93dbfc76bb9f93
co2f2e <48397609+co2f2e@users.noreply.github.com>
2024-11-10 20:02:35 +0800

    Update Theme.js

--- plugins/Theme.js
+++ plugins/Theme.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let style = document.createElement("style");
     style.innerHTML = `
         html {    
-            background: url('https://blog.freeblock.cn/background.webp') no-repeat center center fixed;
+            background: url('https://cdn.jsdelivr.net/gh/co2f2e/co2f2e.github.io@main/static/images/background.webp') no-repeat center center fixed;
             background-size: cover;
         }
 
