84b78bac61a7270a30295c684a6b59f7a44d65c4
co2f2e <48397609+co2f2e@users.noreply.github.com>
2024-11-10 18:19:09 +0800

    Update Theme.js

--- plugins/Theme.js
+++ plugins/Theme.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {    
     let currentUrl = window.location.pathname;
 
-    // 创建样式
     let style = document.createElement("style");
     style.innerHTML = `
     html {    
-        background: url('https://blog.freeblock.cn/background.webp') no-repeat center center fixed;
+        background: url('https://cdn.jsdelivr.net/gh/co2f2e/co2f2e.github.io@main/static/images/background.webp') no-repeat center center fixed;
         background-size: cover;
     }
 
