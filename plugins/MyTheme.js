3c1418d0a1dd42193982129fd30f1700edee8020
co2f2e <48397609+co2f2e@users.noreply.github.com>
2024-11-10 21:20:36 +0800

    Update and rename Theme.js to MyTheme.js

--- plugins/MyTheme.js
+++ plugins/MyTheme.js
@@ -5,36 +5,36 @@ document.addEventListener('DOMContentLoaded', function() {
     let style = document.createElement("style");
     style.innerHTML = `
         /* Header 样式 */
-#header {
-    height: 250px; /* 适当降低 header 高度 */
-    position: relative; /* 确保内部的绝对定位生效 */
+        #header {
+            height: 250px; /* 适当降低 header 高度 */
+            position: relative; /* 确保内部的绝对定位生效 */
 }
 
-/* 标题样式 */
-#header h1 {
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: fantasy;
+        /* 标题样式 */
+        #header h1 {
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            font-family: fantasy;
 }
 
-/* 头像样式 */
-.avatar {
-    width: 150px;
-    height: 150px;
-    border-radius: 50%; /* 使头像变为圆形 */
-    border: 3px solid #fff; /* 添加白色边框 */
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* 添加阴影 */
+        /* 头像样式 */
+        .avatar {
+            width: 150px;
+            height: 150px;
+            border-radius: 50%; /* 使头像变为圆形 */
+            border: 3px solid #fff; /* 添加白色边框 */
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* 添加阴影 */
 }
 
-/* 标题文字样式 */
-#header h1 a {
-    margin-top: 20px; /* 头像与标题的间距 */
-    color: #333;
-    text-decoration: none;
+        /* 标题文字样式 */
+        #header h1 a {
+            margin-top: 20px; /* 头像与标题的间距 */
+            color: #333;
+            text-decoration: none;
 }
         html {    
             background: url('https://cdn.jsdelivr.net/gh/co2f2e/co2f2e.github.io@main/static/images/background.webp') no-repeat center center fixed;
